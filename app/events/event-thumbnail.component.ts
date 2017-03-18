8713c769538ed4fbe419f045f246f7c45134b653
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-18 19:42:12 -0400

    clean up

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,10 +6,5 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event:any
-
-  someProperty:any = "some value"
-
-  logFoo() {
-    console.log('foo')
-  }
+  
 }
