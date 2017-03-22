0f2cce7fcbdec8446d79266781136fc7ad9a3646
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-22 17:19:38 -0400

    styling components with class binding

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,6 +4,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   templateUrl: './app/events/event-thumbnail.component.html',
   styles: [`
+    .green { color: #003300 !important; }
     .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
