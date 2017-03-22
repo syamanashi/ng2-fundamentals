181f3bafeda0d8764c472b405df79bbc7759e53c
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-22 17:59:44 -0400

    styling components with ngStyle (calling a function)

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,8 +4,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   templateUrl: './app/events/event-thumbnail.component.html',
   styles: [`
-    .green { color: #003300 !important; }
-    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
@@ -14,4 +12,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent {
   @Input() event:any
   
+  getStartTimeStyle():any {
+    if (this.event.time && this.event.time === '8:00 am')
+      return { color: '#003300', 'font-weight': 'bold'}
+    return {}
+  }
 }
