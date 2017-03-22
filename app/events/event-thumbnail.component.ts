2a595722bc24d08b2bb701443a0388878ba474b1
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-22 17:48:12 -0400

    clean up

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -13,20 +13,5 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event:any
-
-  getStartTimeClass() {
-    const isEarlyStart = this.event && this.event.time === '8:00 am';
-    return {green: isEarlyStart, bold: isEarlyStart};
-  }
-  getStartTimeClass_altString() {
-    if (this.event && this.event.time === '8:00 am')
-      return 'green bold';
-    return '';
-  }
-  getStartTimeClass_altArray() {
-    if (this.event && this.event.time === '8:00 am')
-      return ['green', 'bold'];
-    return [];
-  }
   
 }
