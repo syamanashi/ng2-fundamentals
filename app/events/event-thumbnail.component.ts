f626a491e1448f56d7546eb25ee8761864cf39af
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-22 15:54:10 -0400

    - repeating data with *ngFor. - handling NULL values with the safe-navigation operator (?). - hiding and showing content with *ngIf.

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,8 +4,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   templateUrl: './app/events/event-thumbnail.component.html',
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
-    .well div { color: #bbb }
+    .well div { color: #bbb; }
   `]
 })
 export class EventThumbnailComponent {
--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styles: [`
     .nav.navbar-nav { font-size: 15px; }
     #searchForm { margin-right: 100px; }
-    @media (max-width: 1200px) { #searchForm { display: none; } }
+    @media (max-width: 1200px) { #searchForm { display: none; }  }
   `]
 })
 export class NavBarComponent {
