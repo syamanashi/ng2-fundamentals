794bde34f1ae91acde5e59c8de6067b3be8a748d
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-23 18:27:58 -0400

    Routing and Navigating Pages - pre-loading data for components (using a Resolve route handler) so that nothing in the component loads until all resolved data is received.

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 import { ToastrService } from '../common/toastr.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   templateUrl: './app/events/events-list.component.html',
@@ -9,11 +10,16 @@ export class EventsListComponent implements OnInit {
   eventListHeading = "Upcoming Angular 2 Events"
   events:any
 
-  constructor(private eventService: EventService, private toastr:ToastrService) {
+  constructor(
+    private eventService: EventService,
+    private toastr:ToastrService,
+    private route:ActivatedRoute) {
+
   }
 
   ngOnInit() {
-    this.eventService.getEvents().subscribe(events => this.events = events)
+    // this.eventService.getEvents().subscribe(events => this.events = events)
+    this.events = this.route.snapshot.data['events']
   }
 
   handleThumbnailClick(eventName) {
