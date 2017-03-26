a9bd37eaed894f43be1c614dc84119fbc93926dc
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-26 14:25:03 -0400

    Collecting Data with Form and Validation - using models for type safey

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 import { ToastrService } from '../common/toastr.service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/index'
 
 @Component({
   templateUrl: './app/events/events-list.component.html',
 })
 export class EventsListComponent implements OnInit {
   eventListHeading = "Upcoming Angular 2 Events"
-  events:any
+  events:IEvent[]
 
   constructor(
     private eventService: EventService,
