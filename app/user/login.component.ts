4083d9e5a76c65e6825a1aeeb865051015776a8c
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-26 14:47:27 -0400

    Collecting Data with Form and Validation - creating your first template-based form

---
+++ app/user/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl: 'app/user/login.component.html'
+})
+export class LoginComponent {
+  login(formValues) {
+    console.log(formValues)
+  }
+}
