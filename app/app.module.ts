751270675ba1cf7f13ec8c6b46fe7830134c82f0
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-29 12:14:45 -0400

    Collecting Data with Form and Validation: diving deeper into template-based forms - Import the FormsModule into AppModule (Recall that FormsModule was previously imported into UserModule) - Replace the html for the CreateEventComponent with Template-based Form html - Copy styles from ProfileComponent - Add saveEvent() method that logs out the formValues to the console

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 
 import {
     EventsListComponent,
@@ -22,6 +23,7 @@ import { AuthService } from './user/auth.service'
 @NgModule ({
     imports: [
         BrowserModule,
+        FormsModule,
         RouterModule.forRoot(appRoutes),
     ],
     declarations: [
