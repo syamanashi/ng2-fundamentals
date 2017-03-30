d3d59051b3fef3709411cf8fcb3492a9b7bb7a08
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-30 16:15:58 -0400

    Displaying Data with Pipes: Filtering Data - Add a level filter for event sessions: - Pass the EventDetailsComponent filterBy property into the SessionListComponent with a new property binding to a new [filterBy] property in SessionListComponent - Update SessionListComponent so that it (imports and) implements the interface OnChanges. - Add ngOnChanges() method which implements OnChanges and gets called every time one of the component @Input variables gets a new value, filters sessions accordingly, and sets visibleSessions to be a new array of filtered sessions. - Note: With this code, nothing is run or changes in the SessionListComponent unless one of the level buttons are clicked on the EventDetailsComponent.

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,10 +1,32 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { ISession } from '../shared/index'
 
 @Component({
   selector: 'session-list',
   templateUrl: './app/events/event-details/session-list.component.html',
+  styles: [`
+    collapsible-well h6 {margin-top:-5px; margin-bottom:10px;}
+  `],
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions:ISession[]
+  @Input() filterBy:string
+  visibleSessions:ISession[] = [];
+
+  ngOnChanges() {
+    // if we actually have sessions set, then filter sessions:
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0); // duplicates entire array with all of the same elements.
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter; // any session whose level matches gets returned (as true) in the new visibleSessions array.
+      })
+    }
+  }
 }
