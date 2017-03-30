8dc417c74ceb2b812b0efeb8452948ec0efc4d89
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-30 07:08:21 -0400

    Reusing Components with Content Projection: Multiple Slot Content Projection - To prevent possible conflict with other CSS classes, change the select=".className" of the ng-content tags to select by an attribute [property] selector instead of className selector. - Remove the @Input title from CollapsibleWellComponent as it's no longer needed.

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'collapsible-well',
   template:`
 <div (click)="toggleContent()" class="well pointable">
   <h4>
-    <ng-content select=".title"></ng-content>
+    <ng-content select="[well-title]"></ng-content>
   </h4>
-  <ng-content *ngIf="visible" select=".body"></ng-content>
+  <ng-content *ngIf="visible" select="[well-body]"></ng-content>
 </div>
   `,
 })
 export class CollapsibleWellComponent {
-
-  @Input() title: string;
   visible: boolean = true;
 
   toggleContent() {
