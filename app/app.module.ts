e4b6b3277cac7a773b88f5aabdac25cb11d552ad
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-30 06:50:38 -0400

    Reusing Components with Content Projection: Content Projection - Add click handler to the sessions page to collapse a session detail well upon being clicked to just show the session title. - Implement with a new reusable component (CollapsibleWellComponent) that handles a dynamic title and passes through (content projection) content between the tags of the new component using <ng-content></ng-content>. - Import the new CollapsibleWellComponent into app.module by creating a new common/index barrel then add it to the declarations. - Note: You may have to restart your dev server so that the new <collapsible-well> tag will get registered properly.

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,7 +17,7 @@ import {
 
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { ToastrService } from './common/toastr.service'
+import { ToastrService, CollapsibleWellComponent } from './common/index';
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
@@ -39,6 +39,7 @@ import { AuthService } from './user/auth.service'
         Error404Component,
         CreateSessionComponent,
         SessionListComponent,
+        CollapsibleWellComponent,
     ],
     providers: [
         EventService,
---
+++ app/common/collapsible-well.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'collapsible-well',
+  template:`
+<div (click)="toggleContent()" class="well pointable">
+  <h4 class="well-title">{{title}}</h4>
+  <!-- content projection: -->
+  <ng-content *ngIf="visible"></ng-content>
+</div>
+  `,
+})
+export class CollapsibleWellComponent {
+
+  @Input() title: string;
+  visible: boolean = true;
+
+  toggleContent() {
+    this.visible = !this.visible;
+  }
+
+}
