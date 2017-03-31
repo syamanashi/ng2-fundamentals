1136fb7cf113b793bd6e61b0d5e14bba4c09b13c
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-31 07:16:28 -0400

    Understanding Dependency Injection: The useExisting Provider - When registering services, using the 'useExisting' Alias Provider can be used to setup an alias class with a few methods you want other programmers - You may (rarely) want to do this to minimize the class API that gets displayed in your editor's intellisence. - { provider: MinimalLogger, useExisting: Logger }

--- app/app.module.ts
+++ app/app.module.ts
@@ -47,7 +47,7 @@ declare let toastr: Toastr
     ],
     providers: [
         EventService,
-        { provide: TOASTR_TOKEN, useValue: toastr },
+        { provide: TOASTR_TOKEN, useValue: toastr }, // => useExisting Alias Provider exampple: { provider: TOASTR_TOKEN, useExisting: toastr }
         EventRouteActivator, // => Shorthand for { provide: EventRouteActivator, useClass: EventRouteActivator } where provide sets the token.
         EventListResolver, // => You could send a different service/class in the longhand syntax like this { provide: EventListResolver, useClass: EventService }
         AuthService,
